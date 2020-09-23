5fe442fe022eaf0858866cc829df2c364500ad0f
Surma <surma@surma.dev>
2020-09-23 15:57:31 +0100

    Run module worker tests only on feature detection

--- tests/module-worker.test.js
+++ tests/module-worker.test.js
@@ -11,6 +11,27 @@
  * limitations under the License.
  */
 
+function jsBlobFromString(s) {
+  return URL.createObjectURL(new Blob([s], { type: "text/javascript" }));
+}
+
+// If `new Worker()` checks the `type` prop on the options object
+// we can be fairly sure that the browser supports module workers.
+// If so, replace we can switch `maybeIt` from ignoring tests (`xit`)
+// to actually running the tests (`it`).
+let maybeIt = xit;
+let wasRead = false;
+const detector = {
+  get type() {
+    wasRead = true;
+    return "module";
+  }
+};
+new Worker(jsBlobFromString("export default 4"), detector).terminate();
+if (wasRead) {
+  maybeIt = it;
+}
+
 describe("Comlink plugin", function() {
   beforeEach(function() {
     this.ifr = document.createElement("iframe");
@@ -21,7 +42,7 @@ describe("Comlink plugin", function() {
     this.ifr.remove();
   });
 
-  it("proxies module workers", function(done) {
+  maybeIt("proxies module workers", function(done) {
     window.addEventListener("message", function l(ev) {
       if (ev.data === "a") {
         window.removeEventListener("message", l);
