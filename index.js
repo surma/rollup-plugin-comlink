e8054d6b280606d86ff61f712f95907d58d47ac1
Ada Rose Cannon <4225330+AdaRoseCannon@users.noreply.github.com>
2021-06-16 18:26:57 +0100

    Fix new Worker() path
    Work out the import path of the current script on run time and prepend it to the importing URL so that it still works when the build directory is different from the HTML page loading the file.

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function generateLoaderModule(path, { useModuleWorker = false } = {}) {
 		import workerPath from ${JSON.stringify(`omt:${path}`)};
 		import {wrap} from "comlink";
 
-		export default wrap(new Worker(workerPath${
+		const currentImportPath = (new URL(import.meta.url).pathname.match(/(^\/.+)*\//g) || [""])[0];
+		export default wrap(new Worker(currentImportPath + workerPath${
       useModuleWorker ? `, {type: "module"}` : ""
     }));
 	`;
